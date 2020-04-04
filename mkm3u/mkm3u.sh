138ec660ad655065193d61f7718488c305edfd90
valerino <xoanino@gmail.com>
2020-04-04 16:24:02 +0200

    fix

--- mkm3u/mkm3u.sh
+++ mkm3u/mkm3u.sh
@@ -2,14 +2,18 @@
 function usage {
     echo 'extract all files matching substring and create .m3u\n'
     echo 'usage:' "$0" '-f <path/to/substring> -d </path/to/destination> [-n m3u name] [-x delete destination first] [-s to skip extract for already unzipped discs]'
-    echo '\t[-l limit to n entries, i.e. 10]\n'
+    echo '\t[-m include only files matching mask, i.e. *.zip] [-l limit to n entries, i.e. 10]\n'
     echo 'if -n is not provided, the provided substring is the name of the generated .m3u'
 }
 _DELETE_DEST=0
 _SKIP_EXTRACT=0
 _LIMIT=0
-while getopts "xsf:d:n:l:" arg; do
+_MASK="*"
+while getopts "xsf:d:n:l:m:" arg; do
     case $arg in
+        m)
+          _MASK="${OPTARG}"
+          ;;
         l)
           _LIMIT="${OPTARG}"
           ;;
@@ -39,6 +43,7 @@ if [ "$_PATH" == "" ] || [ "$_DEST" == "" ]; then
   usage
   exit 1
 fi
+
 
 # remove destination first if asked to
 if [ $_DELETE_DEST -eq 1 ]; then
@@ -87,14 +92,15 @@ echo '[.] generating m3u:' "$_M3UPATH"
 
 if [ $_SKIP_EXTRACT -eq 0 ]; then
   # default, find in the extracted folder
-  find "$_DEST"/* > ./tmp.txt
+  find "$_DEST"/* -maxdepth 1 -iname "$_MASK" > ./tmp.txt
 else
   # use the source folder directly 
-  find -maxdepth 1 "$_parent" | grep "$_tofind" > ./tmp.txt
+  find "$_parent" -maxdepth 1 -iname "$_MASK" | grep "$_tofind" > ./tmp.txt
 fi
 
 # sort and done
 sort ./tmp.txt > ./tmp2.txt
+rm "$_M3UPATH"
 while IFS= read -r line
 do
   echo "$line" >> "$_M3UPATH"
