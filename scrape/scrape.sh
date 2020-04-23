a3f9c7001be5400c9144e9a1b07bad942a83af4c
valerino <xoanino@gmail.com>
2020-04-24 00:41:54 +0200

    fixes

--- scrape/scrape.sh
+++ scrape/scrape.sh
@@ -5,7 +5,8 @@ function usage {
 	echo 'usage' $1 '<-s to scrape|-g to generate gamelist after -s] <-p platform> [-d do not descend in subdirs]'
 	echo '\t[-u screenscraper.fr username:password, expects -s] [-f /path/to/file to scrape single file, expects -s] [-c to refresh cache, expects -s]'
 	echo '\t[-a add extensions space separated i.e. "*.chd *.m3u"] [-x exclude wildcards comma-separated i.e. "*.adz,*.zip"] [-i include wildcards comma-separated i.e. "*.adz,*.zip"]'
-  echo '\t[-t sets roms path, if different than platform] [-n for interactive] [-l relaunch with -g after scraping with -s to generate gamelist.xml in one shot\n'  
+  echo '\t[-t sets roms path, if different than platform] [-n for interactive] [-l relaunch with -g after scraping with -s to generate gamelist.xml in one shot\n'
+  echo '\t[-k to include skipped games anyway]\n'
 	echo 'note: gamelist.xml and media will be created in the platorm folder, gamelist.xml will have relative paths both for roms and media.'
 }
 
@@ -14,11 +15,15 @@ _DO_SCRAPE=0
 _REFRESH_CACHE=0
 _NOSUBDIRS=0
 _INTERACTIVE=0
+_INCLUDE_SKIPPED=0
 _RELAUNCH=0
-while getopts "gnslcu:t:p:f:x:i:a:d" arg; do
+while getopts "gnsklcu:t:p:f:x:i:a:d" arg; do
     case $arg in
         d)
           _NOSUBDIRS=1
+          ;;
+        k)
+          _INCLUDE_SKIPPED=1
           ;;
         t)
           _INPUT_PATH="${OPTARG}"
@@ -97,7 +102,9 @@ fi
 if [ ! -z "$_INPUT_PATH" ]; then
 	set -- "$@" -i "$_INPUT_PATH"
 fi
-
+if [ ! -z "$_INCLUDE_SKIPPED" ]; then
+  set -- "$@" --skipped
+fi
 if [ "$_NOSUBDIRS" -eq 1 ]; then
 	set -- "$@" --nosubdirs
 fi
@@ -137,7 +144,9 @@ if [ "$_RELAUNCH" -eq 1 ]; then
   if [ ! -z "$_ADDEXT" ]; then
 	  set -- "$@" --addext "$_ADDEXT"
   fi
-
+  if [ ! -z "$_INCLUDE_SKIPPED" ]; then
+    set -- "$@" --skipped
+  fi
   if [ ! -z "$_INPUT_PATH" ]; then
 	  set -- "$@" -i "$_INPUT_PATH"
   fi
